eccc4e5eb07be423e5da51c73bd177d8dd6480ae
TaylorUNSA <118939780+TAYTOS@users.noreply.github.com>
2024-04-09 07:26:44 -0500

    casos

--- app/src/components/RollBackSettings.tsx
+++ app/src/components/RollBackSettings.tsx
@@ -96,5 +96,208 @@ const subjects : Subject[] = [
     credits: 4,
   }
 ]
+const subjects_2 : Subject[] = [
+  {
+    name: "PROGRAMACION WEB 2	",
+    id : "1702122",
+    group: "A",
+    mat: 1,
+    credits: 4,
+  },
+  {
+    name: "PROGRAMACION DE SISTEMAS	",
+    id : "1703134",
+    group: "A",
+    mat: 1,
+    credits: 4,
+  },
+  {
+    name: "TEORIA DE LA COMPUTACION	",
+    id : "1703136",
+    group: "A",
+    mat: 1,
+    credits: 3,
+  },
+  {
+    name: "ORGANIZACION Y METODOS	",
+    id : "1703137",
+    group: "A",
+    mat: 1,
+    credits: 3,
+  },
+  {
+    name: "INVESTIGACION DE OPERACIONES	",
+    id : "1703138",
+    group: "A",
+    mat: 1,
+    credits: 4,
+  }
+]
+
+const subjects_3 : Subject[] = [
+  {
+    name: "PROGRAMACION WEB 2	",
+    id : "1702122",
+    group: "C",
+    mat: 1,
+    credits: 4,
+  },
+  {
+    name: "BASE DE DATOS	",
+    id : "1703133",
+    group: "A",
+    mat: 1,
+    credits: 4,
+  },
+  {
+    name: "PROGRAMACION DE SISTEMAS	",
+    id : "1703134",
+    group: "A",
+    mat: 1,
+    credits: 4,
+  },
+  {
+    name: "TEORIA DE LA COMPUTACION	",
+    id : "1703136",
+    group: "A",
+    mat: 1,
+    credits: 3,
+  },
+  {
+    name: "ORGANIZACION Y METODOS	",
+    id : "1703137",
+    group: "A",
+    mat: 1,
+    credits: 3,
+  },
+  {
+    name: "INVESTIGACION DE OPERACIONES	",
+    id : "1703138",
+    group: "A",
+    mat: 1,
+    credits: 4,
+  }
+]
+
+const subjects_4 : Subject[] = [
+  {
+    name: "BASE DE DATOS	",
+    id : "1703133",
+    group: "A",
+    mat: 1,
+    credits: 4,
+  },
+  {
+    name: "PROGRAMACION DE SISTEMAS	",
+    id : "1703134",
+    group: "A",
+    mat: 1,
+    credits: 4,
+  },
+  {
+    name: "TEORIA DE LA COMPUTACION	",
+    id : "1703136",
+    group: "A",
+    mat: 1,
+    credits: 3,
+  },
+  {
+    name: "ORGANIZACION Y METODOS	",
+    id : "1703137",
+    group: "A",
+    mat: 1,
+    credits: 3,
+  },
+  {
+    name: "INVESTIGACION DE OPERACIONES	",
+    id : "1703138",
+    group: "A",
+    mat: 1,
+    credits: 4,
+  }
+]
+
+const subjects_5 : Subject[] = [
+  {
+    name: "PROGRAMACION WEB 2	",
+    id : "1702122",
+    group: "A",
+    mat: 1,
+    credits: 4,
+  },
+  {
+    name: "PROGRAMACION DE SISTEMAS	",
+    id : "1703134",
+    group: "A",
+    mat: 1,
+    credits: 4,
+  },
+  {
+    name: "TEORIA DE LA COMPUTACION	",
+    id : "1703136",
+    group: "A",
+    mat: 1,
+    credits: 3,
+  },
+  {
+    name: "ORGANIZACION Y METODOS	",
+    id : "1703137",
+    group: "A",
+    mat: 1,
+    credits: 3,
+  },
+  {
+    name: "INVESTIGACION DE OPERACIONES	",
+    id : "1703138",
+    group: "A",
+    mat: 1,
+    credits: 4,
+  }
+]
+const subjects_6 : Subject[] = [
+  {
+    name: "PROGRAMACION WEB 2	",
+    id : "1702122",
+    group: "A",
+    mat: 1,
+    credits: 4,
+  },
+  {
+    name: "BASE DE DATOS	",
+    id : "1703133",
+    group: "B",
+    mat: 1,
+    credits: 4,
+  },
+  {
+    name: "PROGRAMACION DE SISTEMAS	",
+    id : "1703134",
+    group: "B",
+    mat: 1,
+    credits: 4,
+  },
+  {
+    name: "TEORIA DE LA COMPUTACION	",
+    id : "1703136",
+    group: "B",
+    mat: 1,
+    credits: 3,
+  },
+  {
+    name: "ORGANIZACION Y METODOS	",
+    id : "1703137",
+    group: "B",
+    mat: 1,
+    credits: 3,
+  },
+  {
+    name: "INVESTIGACION DE OPERACIONES	",
+    id : "1703138",
+    group: "B",
+    mat: 1,
+    credits: 4,
+  }
+]
+
 
 export default RollBackSettings
